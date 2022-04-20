ff628e6382f8a1d7992d66735235941bc78d79a3
hinduja'afarudanlam <77009428+haseenaisahibraheem@users.noreply.github.com>
2022-04-20 09:11:06 +0000

    initial commit

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,38 +1,44 @@
-import React from 'react'
+import React from "react";
 
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
-import './styles.css';
+import "./styles.css";
 
 function TeacherItem() {
-    return (
-        <article className="teacher-item">
-            <header>
-                <img src="https://avatars3.githubusercontent.com/u/61950037?s=460&u=7ac31ef71a58643743b786080ea3d37aea2e99d2&v=4" alt="Ciro Lopes" />
-                <div>
-                    <strong>Ciro Lopes</strong>
-                    <span>Química</span>
-                </div>
-            </header>
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars3.githubusercontent.com/u/61950037?s=460&u=7ac31ef71a58643743b786080ea3d37aea2e99d2&v=4"
+          alt="MUHAMMAD MANNIR"
+        />
+        <div>
+          <strong>MUHAMMAD MANNIR</strong>
+          <span>AHMAD</span>
+        </div>
+      </header>
 
-            <p>
-                Entusiasta das melhores tecnologias de química avançada.
-                    <br /><br />
-                Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200 mil pessoas ja passaram por uma das minhas explosões.
-            </p>
+      <p>
+        MANNIR e_SYSTEMS NIGERIA LIMITED,TEAM WORK IS THE STRENGTH.
+        <br />
+        <br />
+        We have developed comprehensive and innovative solutions for a wide
+        variety of business,while we develop system our focus is on customer"s
+        need.
+      </p>
 
-            <footer>
-                <p>
-                    Preço/Hora
-                    <strong>R$ 80,00</strong>
-                </p>
-                <button type="button">
-                    <img src={whatsappIcon} alt="Entrar em contato" />
-                            Entrar em contato
-                </button>
-            </footer>
-        </article>
-    );
+      <footer>
+        <p>
+          price/Other Name
+          <strong>R$ 80,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Contact us here" />
+          Contact us here
+        </button>
+      </footer>
+    </article>
+  );
 }
 
 export default TeacherItem;
